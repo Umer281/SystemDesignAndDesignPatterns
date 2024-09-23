dee943df23dd2d925bd1d232456061e9dd7c9cf8
Umer281 <umerbaba86@gmail.com>
2024-09-23 17:17:45 +0530

    changes

--- Node Js/server/index.js
+++ Node Js/server/index.js
@@ -3,6 +3,24 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use(express.urlencoded({extended: true}));
+
+//middlwares
+
+
+app.use((req,res, next) => {
+    console.log("iniide middlwares 1");
+     if(req.query.token) {
+          req.query.isValidUser = true;
+     }
+    next();
+});
+
+app.use((req,res, next) => {
+    console.log(req.query.isValidUser, "user1");
+    next();
+});
+
 //routes 
 require('./route/index')(app);  
 
